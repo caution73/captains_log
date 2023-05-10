ed230bdfdaa89bdf80670855d9039f08c9fa0bfd
Devin Hansen <hansdevi@isu.edu>
2023-05-10 18:08:08 -0500

    Added New.jsx with basic form, created New route in server. Added middleware.

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const db = mongoose.connection
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Connection Error/Success
+// Define callback functions for various events
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('open', () => console.log('mongo connected'));
+db.on('close', () => console.log('mongo disconnected'));
+
+module.exports = db;
---
+++ views/New.jsx
@@ -0,0 +1,22 @@
+const React = require("react")
+
+class New extends React.Component {
+    render(){
+        return(
+            <div className="formContainer">
+                <form action="/logs"and method="POST">
+                    <h2>Title</h2>
+                    <input type="text" name="title"></input>
+                    <h3>Entry</h3>
+                    <input type="textarea" name="entry"></input>
+                    <h3>Ship is damaged</h3>
+                    <input type="checkbox" name="shipIsBroken"></input>
+                    <input type="submit"></input>
+                </form>
+            </div>
+           
+        )
+    }
+}
+
+module.exports = New
